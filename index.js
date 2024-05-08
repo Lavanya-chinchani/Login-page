613a176c40117af661793df01ada9ca551d3b8af
Lavanya-chinchani <chinchanilavanya>
2024-05-08 10:48:50 +0530

    first

---
+++ index.js
@@ -0,0 +1,52 @@
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      username: '',
+      password: '',
+      errorMessage: '',
+      rememberMe: false,
+      showPassword: false
+    };
+  },
+  computed: {
+    visibilityIcon() {
+      return this.showPassword ? 'visibility' : 'visibility_off';
+    }
+  },
+  methods: {
+    toggleVisibility() {
+      this.showPassword = !this.showPassword;
+    },
+    login() {
+      if (!this.username.trim() && !this.password.trim()) {
+        alert('Please fill in both username/email and password fields.');
+      } else if (!this.username.trim() || !this.password.trim()) {
+        if (!this.username.trim()) {
+          alert('Please fill in the username/email field.');
+        }
+        if (!this.password.trim()) {
+          alert('Please fill in the password field.');
+        }
+      } else {
+        alert('Successfully logged in!');
+      }
+    },
+    forgotPassword() {
+      alert('Forgot Password?');
+    },
+    checkInput() {
+      this.errorMessage = '';
+
+      if (this.username.includes('@')) {
+        if (!this.isValidEmail(this.username)) {
+          this.errorMessage = 'Email format is not correct';
+        }
+      }
+    },
+    isValidEmail(email) {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return emailRegex.test(email);
+    }
+  }
+});
